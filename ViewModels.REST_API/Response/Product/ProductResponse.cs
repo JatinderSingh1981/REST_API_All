8cc1a60b7513bad78988975fd12bf050d43db06f
J Singh <singh.jatinder@hotmail.com>
2021-01-28 20:57:41 +1100

    Removed comments

--- ViewModels.REST_API/Response/Product/ProductResponse.cs
+++ ViewModels.REST_API/Response/Product/ProductResponse.cs
@@ -2,13 +2,6 @@
 
 namespace ViewModels.REST_API
 {
-    //public class ProductResponse<T> where T : Product
-    //{
-    //    public T Product { get; set; }
-    //    public bool IsSuccess { get; set; }
-    //    public string Message { get; set; }
-    //}
-
     public class ProductResponse
     {
         public Product Product { get; set; }
